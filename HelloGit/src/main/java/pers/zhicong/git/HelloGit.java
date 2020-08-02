3956aac83b19b1b32ac9e607cc8e8fd7fa7c192a
zcpers20 <zcpers20@aliyun.com>
2020-08-02 19:52:03 +0800

    更新1

--- HelloGit/src/main/java/pers/zhicong/git/HelloGit.java
+++ HelloGit/src/main/java/pers/zhicong/git/HelloGit.java
@@ -3,5 +3,7 @@ package pers.zhicong.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("更新1");
+
     }
 }
